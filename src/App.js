710634590d4b431aa76c421d351c47366cb7707a
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-04-28 13:52:55 +0200

    Added comments

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 import * as SockJS from 'sockjs-client';
 import * as Stomp from 'stompjs';
 
+// Endpoint for websockets
 const SERVER_URL = 'http://stomp-test.herokuapp.com/ws';
-let socket;
+
+// '/topic/news' is the endpoint for receiving messages (subscriptions)
+// '/app/news' is the endpoint for sending messages
+
 let stompClient;
 
 const App = () => {
@@ -13,15 +17,21 @@ const App = () => {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    const onConnect = () => {
+    // Create new socket from endpoint
+    const socket = new SockJS(SERVER_URL);
+
+    // Create the stomp client instance from socket
+    stompClient = Stomp.over(socket);
+
+    // connect the stomp client
+    // first arg is headers,
+    // second arg is onConnected callback,
+    // third arg is onError callback
+    stompClient.connect({}, () => {
       stompClient.subscribe(`/topic/news`, msg => {
         setResponse(res => [msg.body, ...res]);
       });
-    }
-
-    socket = new SockJS(SERVER_URL);
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, onConnect, err => console.error(err));
+    }, err => console.error(err));
 
     return () => stompClient.disconnect();
   }, []);
@@ -30,6 +40,11 @@ const App = () => {
     if((e.key && e.key !== 'Enter') || e.target.value === '') {
       return;
     }
+    
+    // Send message to the server:
+    // first arg is the endpoint
+    // second arg is the headers
+    // third arg is the data to be sent; in this case a simple string
     stompClient.send('/app/news', {}, message);
     setMessage('');
   }
